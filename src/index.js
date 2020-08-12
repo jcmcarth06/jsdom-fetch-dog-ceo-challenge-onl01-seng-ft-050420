5ed711ed780a12590a7373444d64e51f4b75f1e7
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-12 00:50:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ function getDogs() {
     return fetch(imgUrl).then(resp => resp.json()).then(dogs => renderDogs(dogs));
 };
 
+function getDogs() {
+    return fetch(imgUrl).then(resp => resp.json()).then(dogs => renderDogs(dogs));
+}
+
 function renderDogs(dogs) {
     dogs['message'].forEach(dog => document.getElementById('dog-image-container').innerHTML += `<img src=${dog}>`);
 };
